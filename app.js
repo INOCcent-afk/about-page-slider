fdb5e26cfde11f31336b113c50fbe24cb7ae4751
INOCcent-afk <michael828inoc@gmail.com>
2020-12-09 12:16:22 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,42 @@
+var myAboutPageSwiper = new Swiper(".aboutPage-slider-swiper-container", {
+  speed: 400,
+  slidesPerView: 1.2,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    400: {
+      slidesPerView: 1.5,
+    },
+    450: {
+      slidesPerView: 1.5,
+    },
+    500: {
+      slidesPerView: 1.8,
+    },
+    700: {
+      slidesPerView: 2.2,
+    },
+    1000: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+// /* !!!!!!!!!!!!!!!! THIS IS NOT FOR THE CAROUSEL !!!!!!!!!!!!!!! */
+// /* !!!!!!!!!!!!!!!! THIS IS NOT FOR THE CAROUSEL !!!!!!!!!!!!!!! */
+// /* !!!!!!!!!!!!!!!! THIS IS NOT FOR THE CAROUSEL !!!!!!!!!!!!!!! */
+// /* !!!!!!!!!!!!!!!! THIS IS NOT FOR THE CAROUSEL !!!!!!!!!!!!!!! */
+// /* !!!!!!!!!!!!!!!! THIS IS NOT FOR THE CAROUSEL !!!!!!!!!!!!!!! */
+// /* !!!!!!!!!!!!!!!! THIS IS NOT FOR THE CAROUSEL !!!!!!!!!!!!!!! */
+// /* !!!!!!!!!!!!!!!! THIS IS NOT FOR THE CAROUSEL !!!!!!!!!!!!!!! */
+// /* !!!!!!!!!!!!!!!! THIS IS NOT FOR THE CAROUSEL !!!!!!!!!!!!!!! */
